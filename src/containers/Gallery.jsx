cdda36753d54baa93bcc1b3aeee3fbadf6c839cf
Firdaws Lamidi <lamidifirdaws@gmail.com>
2022-08-16 00:29:17 +0100

    favicon

--- src/containers/Gallery.jsx
+++ src/containers/Gallery.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Navigation } from "swiper";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import { Pagination, Navigation } from "swiper";
 import Instagram from '../components/Instagram'
-import SandwichGallery from '../components/SandwichGallery'
-import leftarrow from '../assets/leftarrow.png'
-import rightarrow from '../assets/rightarrow.png'
-import firstimg from '../assets/firstimg.png'
-import secondimg from '../assets/secondimg.png'
-import thirdimg from '../assets/thirdimg.png'
-import fourthimg from '../assets/fourthimg.png'
-import fifthimg from '../assets/Reubenette.png'
-import sixthimg from '../assets/Porko.png'
-import seventhimg from '../assets/roastbeef.png'
-import eightimg from '../assets/Olepastrami.png'
+// import SandwichGallery from '../components/SandwichGallery'
+// import leftarrow from '../assets/leftarrow.png'
+// import rightarrow from '../assets/rightarrow.png'
+// import firstimg from '../assets/firstimg.png'
+// import secondimg from '../assets/secondimg.png'
+// import thirdimg from '../assets/thirdimg.png'
+// import fourthimg from '../assets/fourthimg.png'
+// import fifthimg from '../assets/Reubenette.png'
+// import sixthimg from '../assets/Porko.png'
+// import seventhimg from '../assets/roastbeef.png'
+// import eightimg from '../assets/Olepastrami.png'
 
 const Gallery = () => {
     
   return (
     <div>
         <Instagram />
-        <div className='relative my-[20px]'>
+        {/* <div className='relative my-[20px]'>
             <div className="flex flex-row">
                 <img src={leftarrow} alt="left arrow" className='absolute top-[20%] left-[3%] cursor-pointer' />
                 <img src={rightarrow} alt="right arrow" className='absolute top-[20%] right-[3%] cursor-pointer' />
@@ -48,7 +48,7 @@ const Gallery = () => {
         <SwiperSlide><SandwichGallery image={eightimg}/></SwiperSlide>
         </Swiper>
        
-    </div>
+    </div> */}
     </div>
   )
 }
