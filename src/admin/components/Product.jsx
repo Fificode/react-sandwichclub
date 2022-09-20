17d68a3bcef836648cc90967abd6cc93ec38f943
Firdaws Lamidi <lamidifirdaws@gmail.com>
2022-09-20 13:00:16 +0100

    product page

--- src/admin/components/Product.jsx
+++ src/admin/components/Product.jsx
@@ -5,14 +5,15 @@ import 'reactjs-popup/dist/index.css';
 const Product = ({ image, name, price}) => {
   return (
     <>
-      <div className="w-[300px] h-[300px] shadow-md mt-[30px]">
+    <h1 className="mx-[10px] font-[600] mt-[30px] text-[25px] md:text-[40px]">{name}</h1>
+      <div className="w-[200px] h-[250px] md:w-[300px] md:h-[300px] shadow-md mt-[30px]">
        <div className="flex flex-row relative ">
-<img src={image} alt={name} className='h-[200px] w-[100%]'/>
-<Popup trigger={<div className="absolute right-0 mt-[5px]"><button><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-6 h-6">
+<img src={image} alt={name} className='h-[150px] md:h-[200px] w-[100%]'/>
+<Popup  trigger={<div className="absolute right-0 mt-[5px]"><button><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
 </svg></button></div>} 
      position="right center" >
-    <div className="flex flex-col items-center">  
+    <div className=" flex flex-col items-center">  
     <div className=""><button className='text-[18px] leading-[29px] font-[600] tracking-[0.0015em] text-black text-center p-1'>Edit</button></div>
       <div><button className='text-[18px] leading-[29px] font-[600] tracking-[0.0015em] text-black text-center p-1'>Delete</button>
       </div>
--- src/admin/containers/Productpage.jsx
+++ src/admin/containers/Productpage.jsx
@@ -10,7 +10,7 @@ const {productId} = useParams();
 const thisProduct = allProducts.find(prod => prod['_id'] === productId)
 // console.log(thisProduct);
   return (
-    <div className='flex flex-col absolute left-[70px] smaller:left-[85px]  md:left-[100px] xl:left-[280px] mt-[20px]'>
+    <div className='flex flex-col absolute left-[90px] smaller:left-[85px]  md:left-[100px] xl:left-[280px] mt-[20px]'>
    
     <Product name={thisProduct.name} image={thisProduct.image} price={thisProduct.price}/>
     
